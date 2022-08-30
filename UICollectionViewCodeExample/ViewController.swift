8a94605d06b3adddbaec0a17c21c66733580be0b
Erika Bueno <erikab@ciandt.com>
2022-08-30 12:30:22 -0300

    Initial Commit

---
+++ UICollectionViewCodeExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionViewCodeExample
+//
+//  Created by Erika C. Matesz Bueno on 30/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
